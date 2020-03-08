ca36291be1ff3046e45b89202247ea82af70baf0
izikodzik <ITI-guuy@gmail.com>
2020-03-08 21:35:58 +0100

    DONE

--- crawler/crawler/Program.cs
+++ crawler/crawler/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace crawler
 {
     class Program
+
+
     {
-        static void Main(string[] args)
+
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(args[0]);
+            var responseAsString = response.ToString();
+
+            Console.WriteLine(responseAsString);
+            Console.WriteLine("\n");
+
+            Regex exp = new Regex("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b", RegexOptions.IgnoreCase);
+            MatchCollection emails = exp.Matches(response);
+            HashSet<String> setMail = new HashSet<string>();
+
+            foreach (Match email in emails) {
+                setMail.Add(email.Value.ToUpper());
+             
+              
+            }
+
+            foreach (String mail in setMail)
+                Console.WriteLine(mail);
+
         }
+
     }
 }
